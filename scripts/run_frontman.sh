d01d54be81bea1fdefecde774b7805097ddf1639
Sebastián Altamirano <38230545+sebastian-altamirano@users.noreply.github.com>
2025-02-15 19:43:41 -0300

    Modify run_frontman.sh so that it works on Windows

--- scripts/run_frontman.sh
+++ scripts/run_frontman.sh
@@ -1,17 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 # Script to run `frontman` in a separate virtual environment to avoid conflicts with the project's
 # dependencies.
 
+set -e
+
+activate_venv() {
+  venv_path="$1"
+  if [ -d "$venv_path/bin" ]; then
+    source "$venv_path/bin/activate"  # Linux and macOS
+  else
+    source "$venv_path/Scripts/activate"  # Windows
+  fi
+}
+
 scripts_path=$(dirname $0)
 frontman_venv_path="$scripts_path/../.venv_frontman"
 
-if [ ! -d $frontman_venv_path ]; then
-	python3.12 -m venv $frontman_venv_path
-	. "$frontman_venv_path/bin/activate"
+if [ ! -d "$frontman_venv_path" ]; then
+	python -m venv $frontman_venv_path
+	activate_venv "$frontman_venv_path"
 	pip install frontman
 else
-	. "$frontman_venv_path/bin/activate"
+	activate_venv "$frontman_venv_path"
 fi
 
 python -m frontman "$@"
